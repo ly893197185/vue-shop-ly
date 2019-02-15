cee705bf99912deb436a9939b2c4d8820b72363d
ly <893197185@qq.com>
2019-02-15 15:40:52 +0800

    登陆功能开发完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -29,8 +29,8 @@ export default {
   data() {
     return {
       loginForm: {
-        username: '',
-        password: ''
+        username: 'admin',
+        password: '123456'
       },
       // 给登陆表单域设置检验规则
       loginFormRules: {
